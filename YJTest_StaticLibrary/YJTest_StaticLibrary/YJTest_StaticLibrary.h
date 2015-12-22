40624964c19bd61a571cd01df93ec0f8264384a5
YJ <yuanjuan@yibaomd.com>
2015-12-22 11:11:35 +0800

    静态库封装测试

---
+++ YJTest_StaticLibrary/YJTest_StaticLibrary/YJTest_StaticLibrary.h
@@ -0,0 +1,13 @@
+//
+//  YJTest_StaticLibrary.h
+//  YJTest_StaticLibrary
+//
+//  Created by YJ on 15/12/17.
+//  Copyright © 2015年 YJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YJTest_StaticLibrary : NSObject
+
+@end
---
+++ YJTest_StaticLibrary/YJTest_StaticLibrary/YJTest_StaticLibrary.m
@@ -0,0 +1,13 @@
+//
+//  YJTest_StaticLibrary.m
+//  YJTest_StaticLibrary
+//
+//  Created by YJ on 15/12/17.
+//  Copyright © 2015年 YJ. All rights reserved.
+//
+
+#import "YJTest_StaticLibrary.h"
+
+@implementation YJTest_StaticLibrary
+
+@end
