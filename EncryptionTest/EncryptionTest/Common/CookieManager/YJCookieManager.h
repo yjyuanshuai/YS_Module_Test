6e7c974ec2ee9c98ded33903b63cd22a27d9d290
YJ <yuanjuan@yibaomd.com>
2016-01-04 16:25:02 +0800

    Cookie增加和删除

---
+++ EncryptionTest/EncryptionTest/Common/CookieManager/YJCookieManager.h
@@ -0,0 +1,13 @@
+//
+//  YJCookieManager.h
+//  EncryptionTest
+//
+//  Created by YJ on 16/1/4.
+//  Copyright © 2016年 YJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YJCookieManager : NSObject
+
+@end
