36b1eddf8716fffbe6616ffa114c837cf2633098
YJ <yuanjuan@yibaomd.com>
2016-01-05 11:30:22 +0800

    基于AFNetWork的封装

---
+++ YiBaoHttpRequestTest/YiBaoHttpRequestTest/Common/NetWork/NetInterface.h
@@ -0,0 +1,13 @@
+//
+//  NetInterface.h
+//  YiBaoHttpRequestTest
+//
+//  Created by YJ on 16/1/5.
+//  Copyright © 2016年 YJ. All rights reserved.
+//
+
+#ifndef NetInterface_h
+#define NetInterface_h
+
+
+#endif /* NetInterface_h */
