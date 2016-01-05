99c5a7ace682ce0e6b238fd3055c85b078bf904b
YJ <yuanjuan@yibaomd.com>
2016-01-05 16:34:36 +0800

    runtime

---
+++ RuntimeTest/RuntimeTest/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  RuntimeTest
+//
+//  Created by YJ on 16/1/5.
+//  Copyright © 2016年 YJ. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface FirstViewController : RootViewController
+
+@end
