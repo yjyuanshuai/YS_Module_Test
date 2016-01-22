25360cb846f2f59efbdec31b1469a14863e7a663
YJ <yuanjuan@yibaomd.com>
2016-01-22 15:32:44 +0800

    no message

--- 二维码生成与扫描Test/二维码生成与扫描Test/Classes/Items/Main/YSRootViewController.m
+++ 二维码生成与扫描Test/二维码生成与扫描Test/Classes/Items/Main/YSRootViewController.m
@@ -25,26 +25,24 @@ static CGFloat moreHeight = 50;
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    // 生成二维码
     [YSQRCodeImageView qrImageViewWithFrame:CGRectMake((ScreenWidth - 200)/2, 20+moreHeight, 200, 200) String:@"https://www.baidu.com" viewController:self];
     
+    
+    // 生成条形码
     [YSBarCodeImageView bcImageViewWithFrame:CGRectMake((ScreenWidth - 150)/2, 250+moreHeight, 150, 50) string:@"6926292924425" viewController:self];
     
     
+    // 显示扫描得到的二维码/条形码内容
     _codeLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 350+moreHeight, ScreenWidth - 40, 50)];
     _codeLabel.textColor = [UIColor blackColor];
     _codeLabel.backgroundColor = [UIColor purpleColor];
     [self.view addSubview:_codeLabel];
     
-    UIButton * qrButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    qrButton.frame = CGRectMake(20, 420+moreHeight, 100, 30);
-    qrButton.backgroundColor = [UIColor lightGrayColor];
-    [qrButton setTitle:@"全屏扫描" forState:UIControlStateNormal];
-    [qrButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [qrButton addTarget:self action:@selector(scanAllScreen) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:qrButton];
+    
     
     UIButton * qrButton2 = [UIButton buttonWithType:UIButtonTypeCustom];
-    qrButton2.frame = CGRectMake(ScreenWidth - 130, 420+moreHeight, 100, 30);
+    qrButton2.frame = CGRectMake(40, 420+moreHeight, ScreenWidth - 80, 30);
     qrButton2.backgroundColor = [UIColor lightGrayColor];
     [qrButton2 setTitle:@"正方形扫描" forState:UIControlStateNormal];
     [qrButton2 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
@@ -54,17 +52,6 @@ static CGFloat moreHeight = 50;
 }
 
 #pragma mark - 扫描
-/**
- *  全屏扫描
- */
-- (void)scanAllScreen
-{
-    
-}
-
-/**
- *  正方形扫描
- */
 - (void)scanSquare
 {
     YSSquareScanCodeViewController * ysSquareVC = [[YSSquareScanCodeViewController alloc] initWithBlock:^(NSString * codeText) {
