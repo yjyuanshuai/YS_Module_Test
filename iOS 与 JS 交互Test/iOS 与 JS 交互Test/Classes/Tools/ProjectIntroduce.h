0a5f68964355d20c9283b5387269d7f0886e2d97
YJ <yuanjuan@yibaomd.com>
2016-01-26 11:09:12 +0800

    JS 与 iOS 交互

---
+++ iOS 与 JS 交互Test/iOS 与 JS 交互Test/Classes/Tools/ProjectIntroduce.h
@@ -0,0 +1,26 @@
+//
+//  ProjectIntroduce.h
+//  iOS 与 JS 交互Test
+//
+//  Created by YJ on 16/1/26.
+//  Copyright © 2016年 YJ. All rights reserved.
+//
+
+#ifndef ProjectIntroduce_h
+#define ProjectIntroduce_h
+
+/**
+    工程说明：
+        
+    主要目的：       测试 iOS 与 JS 交互。
+    导入系统框架：    javaScriptCore.framework
+ 
+    主要内容: 1、iOS 调用 JS
+             2、JS 调用iOS (分两种)
+                1）JS 里直接调用
+                2）JS 通过对象调用方法
+    
+ 
+ */
+
+#endif /* ProjectIntroduce_h */
---
+++ iOS 与 JS 交互Test/iOS 与 JS 交互TestTests/iOS___JS___TestTests.m
@@ -0,0 +1,39 @@
+//
+//  iOS___JS___TestTests.m
+//  iOS 与 JS 交互TestTests
+//
+//  Created by YJ on 16/1/26.
+//  Copyright © 2016年 YJ. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOS___JS___TestTests : XCTestCase
+
+@end
+
+@implementation iOS___JS___TestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
