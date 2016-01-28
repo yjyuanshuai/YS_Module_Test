e86847403d3c5d6548aa9b26bf9fde0d486a6755
YJ <yuanjuan@yibaomd.com>
2016-01-28 17:59:31 +0800

    no message

--- YSCircleImagesScrollView/YSCircleImagesScrollView/Classes/Libs/YSCircleImageDisplayView.h
+++ YSCircleImagesScrollView/YSCircleImagesScrollView/Classes/Libs/YSCircleImageDisplayView.h
@@ -10,4 +10,23 @@
 
 @interface YSCircleImageDisplayView : UIView
 
+#pragma mark - 属性
+#pragma mark -
+@property (nonatomic, assign) NSTimeInterval * timeInterval;    //每张图轮播的时间
+@property (nonatomic, strong) NSArray * imagesArr;              //图片数组
+
+/**
+ *  自定义 pageControl 的图片
+ */
+@property (nonatomic, strong) UIImage * pageControlCurrentImage;    //pageControl选中时的图片
+@property (nonatomic, strong) UIImage * pageControlTintImage;       //pageControl未选中时的图片
+
+
+#pragma mark - 实例方法
+#pragma mark -
+- (instancetype)initWithFrame:(CGRect)frame imagesArr:(NSArray *)imagesArr;
+
+#pragma mark - 类方法
+#pragma mark -
+
 @end
--- YSCircleImagesScrollView/YSCircleImagesScrollView/Classes/Libs/YSPageControl.h
+++ YSCircleImagesScrollView/YSCircleImagesScrollView/Classes/Libs/YSPageControl.h
@@ -10,4 +10,17 @@
 
 @interface YSPageControl : UIView
 
+#pragma mark - 属性
+@property (nonatomic, assign) NSUInteger numberOfPages;   //全部页数
+@property (nonatomic, assign) NSUInteger currentPageNumber; //当前页码
+
+@property (nonatomic, strong) UIImage * pageControlTintImage;       // 未选中时的图片
+@property (nonatomic, strong) UIImage * pageControlCurrentImage;    // 已选中时的图片
+
+@property (nonatomic, strong) UIColor * pageControlTintColor;     //未颜色
+@property (nonatomic, strong) UIColor * pageControlCurrentColor;  //已颜色
+
+#pragma mark - 实例方法
+- (instancetype)initWithFrame:(CGRect)frame;
+
 @end
