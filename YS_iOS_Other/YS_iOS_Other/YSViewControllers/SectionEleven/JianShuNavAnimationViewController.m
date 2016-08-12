96b10847011efff4fec159078a7e08f0558be1eb
YJ <yuanjuan@yibaomd.com>
2016-08-12 18:02:05 +0800

    no message

--- YS_iOS_Other/YS_iOS_Other/YSViewControllers/SectionEleven/JianShuNavAnimationViewController.m
+++ YS_iOS_Other/YS_iOS_Other/YSViewControllers/SectionEleven/JianShuNavAnimationViewController.m
@@ -9,6 +9,9 @@
 #import "JianShuNavAnimationViewController.h"
 #import "UIImage+YSImageCategare.h"
 
+static CGFloat maxSize = 100;
+static CGFloat minSize = 36;
+
 static NSString * const preNavBarTitleTextAttributes    = @"preNavBarTitleTextAttributes";
 static NSString * const preNavBarStyle                  = @"preNavBarStyle";
 static NSString * const preNavBarTintColor              = @"preNavBarTintColor";
@@ -94,19 +97,17 @@ static NSString * const preNavBarBackIndicatorTransitionMaskImage = @"preNavBarB
     UIBarButtonItem * rightBtn = [[UIBarButtonItem alloc] initWithTitle:@"切换效果" style:UIBarButtonItemStylePlain target:self action:@selector(changeEffect)];
     self.navigationItem.rightBarButtonItem = rightBtn;
     
-    UIView * headView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 44, 44)];
+    UIView * headView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 44)];
     headView.backgroundColor = [UIColor orangeColor];
     
     _headImageView = [UIImageView new];
-    _headImageView.image = [UIImage imageNamed:@"11"];
+    _headImageView.bounds = CGRectMake(0, 0, 80, 80);
+    _headImageView.center = CGPointMake(headView.center.x, 44);
+    NSString * path = [[NSBundle mainBundle] pathForResource:@"11" ofType:@"jpg"];
+    _headImageView.image = [UIImage imageWithContentsOfFile:path];
     _headImageView.layer.cornerRadius = 80/2;
     _headImageView.clipsToBounds = YES;
     [headView addSubview:_headImageView];
-    [_headImageView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.size.mas_equalTo(CGSizeMake(80, 80));
-        make.centerX.equalTo(headView).bottom.with.offset(0);
-        make.centerY.equalTo(headView).with.offset()0;
-    }];
     
     self.navigationItem.titleView = headView;
 }
@@ -142,7 +143,7 @@ static NSString * const preNavBarBackIndicatorTransitionMaskImage = @"preNavBarB
     _effectTableView.delegate = self;
     _effectTableView.dataSource = self;
     _effectTableView.hidden = YES;
-    _effectTableView.rowHeight = 30;
+    _effectTableView.rowHeight = 40;
     _effectTableView.scrollEnabled = NO;
     [self.view addSubview:_effectTableView];
     [_effectTableView mas_makeConstraints:^(MASConstraintMaker *make) {
@@ -203,8 +204,14 @@ static NSString * const preNavBarBackIndicatorTransitionMaskImage = @"preNavBarB
 }
 
 #pragma mark - UIScrollViewDelegate
+
+/**
+ *  scrollView滚动时，就调用该方法。任何offset值改变都调用该方法。即滚动过程中，调用多次
+ */
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
+    NSLog(@"------------- scrollViewDidScroll");
+    
     if (_touchNum == 1) {
         // 颜色
         CGFloat miniAlphaOffset = 0;
@@ -232,12 +239,132 @@ static NSString * const preNavBarBackIndicatorTransitionMaskImage = @"preNavBarB
             [self.navigationController setNavigationBarHidden:NO animated:YES];
             
         }
-
     }
-    else {
+    else if (_touchNum == 3) {
         // 缩放
         
+        CGFloat contentY = scrollView.contentOffset.y;
+        CGFloat contentInsertTop = _jianshuTableView.contentInset.top;
+        CGFloat offsetY = contentY + contentInsertTop;
+        CGFloat gesureY = [scrollView.panGestureRecognizer translationInView:_jianshuTableView].y;
+
+        if (_headImageView.frame.size.height >= minSize &&
+            _headImageView.frame.size.height <= maxSize) {
+            
+            CGFloat sy = 1 - offsetY/kScreenHeight;
+            _headImageView.transform = CGAffineTransformMakeScale(sy, sy);
+            
+            CGRect originFrame = _headImageView.frame;
+            originFrame.origin.y = 4;
+            _headImageView.frame = originFrame;
+        }
     }
+}
+
+/**
+ *  当scrollView缩放时，调用该方法。在缩放过程中，回多次调用
+ */
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView{
+    
+    NSLog(@"------------- scrollViewDidScroll");
+    float value=scrollView.zoomScale;
+    NSLog(@"%f",value);
+    
+    
+}
+
+// 当开始滚动视图时，执行该方法。一次有效滑动（开始滑动，滑动一小段距离，只要手指不松开，只算一次滑动），只执行一次。
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView{
+    
+    NSLog(@"------------- scrollViewWillBeginDragging");
+    
+}
+
+// 滑动scrollView，并且手指离开时执行。一次有效滑动，只执行一次。
+// 当pagingEnabled属性为YES时，不调用，该方法
+- (void)scrollViewWillEndDragging:(UIScrollView *)scrollView withVelocity:(CGPoint)velocity targetContentOffset:(inout CGPoint *)targetContentOffset{
+    
+    NSLog(@"------------- scrollViewWillEndDragging");
+    
+}
+
+// 滑动视图，当手指离开屏幕那一霎那，调用该方法。一次有效滑动，只执行一次。
+// decelerate,指代，当我们手指离开那一瞬后，视图是否还将继续向前滚动（一段距离），经过测试，decelerate=YES
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate{
+    
+    NSLog(@"------------- scrollViewDidEndDragging");
+    if (decelerate) {
+        NSLog(@"decelerate");
+    }else{
+        NSLog(@"no decelerate");
+        
+    }
+    
+    CGPoint point=scrollView.contentOffset;
+    NSLog(@"%f,%f",point.x,point.y);
+    
+}
+
+// 滑动减速时调用该方法。
+- (void)scrollViewWillBeginDecelerating:(UIScrollView *)scrollView{
+    
+    NSLog(@"------------- scrollViewWillBeginDecelerating");
+    // 该方法在scrollViewDidEndDragging方法之后。
+    
+    
+}
+
+// 滚动视图减速完成，滚动将停止时，调用该方法。一次有效滑动，只执行一次。
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
+    
+    NSLog(@"------------- scrollViewDidEndDecelerating");
+    
+}
+
+// 当滚动视图动画完成后，调用该方法，如果没有动画，那么该方法将不被调用
+- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView{
+    
+    NSLog(@"------------- scrollViewDidEndScrollingAnimation");
+    // 有效的动画方法为：
+    //    - (void)setContentOffset:(CGPoint)contentOffset animated:(BOOL)animated 方法
+    //    - (void)scrollRectToVisible:(CGRect)rect animated:(BOOL)animated 方法
+    
+    
+}
+
+// 返回将要缩放的UIView对象。要执行多次
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView{
+    
+    NSLog(@"------------- viewForZoomingInScrollView");
+    return nil;
+}
+
+// 当将要开始缩放时，执行该方法。一次有效缩放，就只执行一次。
+- (void)scrollViewWillBeginZooming:(UIScrollView *)scrollView withView:(UIView *)view{
+    
+    NSLog(@"------------- scrollViewWillBeginZooming");
+    
+}
+
+// 当缩放结束后，并且缩放大小回到minimumZoomScale与maximumZoomScale之间后（我们也许会超出缩放范围），调用该方法。
+- (void)scrollViewDidEndZooming:(UIScrollView *)scrollView withView:(UIView *)view atScale:(float)scale{
+    
+    NSLog(@"------------- scrollViewDidEndZooming");
+    
+}
+
+// 指示当用户点击状态栏后，滚动视图是否能够滚动到顶部。需要设置滚动视图的属性：_scrollView.scrollsToTop=YES;
+- (BOOL)scrollViewShouldScrollToTop:(UIScrollView *)scrollView{
+    
+    NSLog(@"------------- scrollViewShouldScrollToTop");
+    return YES;
+    
+}
+
+// 当滚动视图滚动到最顶端后，执行该方法
+- (void)scrollViewDidScrollToTop:(UIScrollView *)scrollView{
+    
+    NSLog(@"------------- scrollViewDidScrollToTop");
 }
 
 #pragma mark -
