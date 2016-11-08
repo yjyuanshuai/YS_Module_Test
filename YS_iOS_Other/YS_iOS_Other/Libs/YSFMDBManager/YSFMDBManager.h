ec35b3da7c34ed23a9e7b2ce46f4a2f684bc981d
YJ <yuanjuan@yibaomd.com>
2016-11-08 10:49:43 +0800

    no message

---
+++ YS_iOS_Other/YS_iOS_Other/Libs/YSFMDBManager/YSFMDBManager.h
@@ -0,0 +1,13 @@
+//
+//  YSFMDBManager.h
+//  YS_iOS_Other
+//
+//  Created by YJ on 16/11/8.
+//  Copyright © 2016年 YJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YSFMDBManager : NSObject
+
+@end
