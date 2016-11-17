176a5f00cc3f1c4947f51665bab10868dbc05b94
YJ <yuanjuan@yibaomd.com>
2016-11-17 17:40:22 +0800

    搜索

--- YS_iOS_Other/YS_iOS_Other/YSViewControllers/YSUI/SearchResultViewController.m
+++ YS_iOS_Other/YS_iOS_Other/YSViewControllers/YSUI/SearchResultViewController.m
@@ -21,26 +21,12 @@
     if (_resultArr == nil) {
         _resultArr = [@[] mutableCopy];
     }
-    
-//    [self createResultTableView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-//
-//- (void)createResultTableView
-//{
-//    _resultTableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
-//    _resultTableView.dataSource = self;
-////    _resultTableView.backgroundColor = [UIColor orangeColor];
-//    [self.view addSubview:_resultTableView];
-//    
-//    [_resultTableView mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.edges.equalTo(self.view);
-//    }];
-//}
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
