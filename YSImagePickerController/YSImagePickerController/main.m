588157891f37bafe01bd082c5b59d5e8af364cda
YJ <yuanjuan@yibaomd.com>
2017-02-14 10:27:20 +0800

    no message

---
+++ YSImagePickerController/YSImagePickerController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YSImagePickerController
+//
+//  Created by YJ on 17/2/14.
+//  Copyright © 2017年 YJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
