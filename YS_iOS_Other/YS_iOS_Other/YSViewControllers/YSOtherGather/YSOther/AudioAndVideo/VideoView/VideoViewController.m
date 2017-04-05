b47c03a3ca6020ec1c62fa42e218da92d325baaa
YJ <yuanjuan@yibaomd.com>
2017-04-05 18:16:22 +0800

    no message

--- YS_iOS_Other/YS_iOS_Other/YSViewControllers/YSOtherGather/YSOther/AudioAndVideo/VideoView/VideoViewController.m
+++ YS_iOS_Other/YS_iOS_Other/YSViewControllers/YSOtherGather/YSOther/AudioAndVideo/VideoView/VideoViewController.m
@@ -7,20 +7,27 @@
 //
 
 #import "VideoViewController.h"
+#import "LandScapeVideoVC.h"
 
-@interface VideoViewController ()
+static NSString * YSVideoControlCellID = @"YSVideoControlCellID";
+
+@interface VideoViewController () <UITableViewDelegate, UITableViewDataSource>
 
 @property (nonatomic, strong) UITableView * videoTableView;
 
 @end
 
 @implementation VideoViewController
+{
+    NSArray * _dataTitleArr;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
     [self initUIAndData];
+    [self createTableView];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -31,8 +38,63 @@
 - (void)initUIAndData
 {
     self.title = @"视频";
+    
+    NSArray * sectionOne = @[@"视频横屏效果"];
+    
+    _dataTitleArr = @[sectionOne];
 }
 
+- (void)createTableView
+{
+    _videoTableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleGrouped];
+    _videoTableView.delegate = self;
+    _videoTableView.dataSource = self;
+    _videoTableView.tableFooterView = [UIView new];
+    [self.view addSubview:_videoTableView];
+    
+    [_videoTableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.view);
+    }];
+    
+    [_videoTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:YSVideoControlCellID];
+}
 
+#pragma mark - UITableViewDelegate, UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return [_dataTitleArr count];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    for (int i = 0; i < [_dataTitleArr count]; i++) {
+        if (i == section) {
+            NSArray * arr = _dataTitleArr[i];
+            return [arr count];
+        }
+    }
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:YSVideoControlCellID];
+    if (indexPath.section < [_dataTitleArr count]) {
+        NSArray * sectionArr = _dataTitleArr[indexPath.section];
+        if (indexPath.row < [sectionArr count]) {
+            cell.textLabel.text = sectionArr[indexPath.row];
+        }
+    }
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.section == 0) {
+        LandScapeVideoVC * landScapeVC = [[LandScapeVideoVC alloc] init];
+        landScapeVC.modalPresentationStyle = UIModalPresentationOverFullScreen;
+        [self presentViewController:landScapeVC animated:YES completion:nil];
+    }
+}
 
 @end
